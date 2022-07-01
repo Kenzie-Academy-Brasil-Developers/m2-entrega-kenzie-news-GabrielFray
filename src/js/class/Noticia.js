6c6235d382a22158dba77398c5a39ae11f0b65a3
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2022-07-01 11:46:52 -0300

    feat: adicionei o resumo novamente

--- src/js/class/Noticia.js
+++ src/js/class/Noticia.js
@@ -8,22 +8,25 @@ export default class Noticia {
     const noticia = document.createElement("article");
     const categoria = document.createElement("span");
     const titulo = document.createElement("h2");
+    const resumo = document.createElement("p");
     const fonte = document.createElement("p");
     const containerInfos = document.createElement("div");
     const imagem = document.createElement("img");
 
     categoria.innerText = dados.categoria;
     titulo.innerText = dados.titulo;
+    resumo.innerText = dados.resumo;
     fonte.innerText = `Fonte: ${dados.fonte}`;
     imagem.src = dados.imagem;
     imagem.alt = "Imagem ilustrativa";
 
     noticia.classList.add("main__article");
     containerInfos.classList.add("info__content");
+    resumo.classList.add("resumo");
     fonte.classList.add("fonte");
 
     mainDeNoticias.append(noticia);
-    containerInfos.append(categoria, titulo, fonte);
+    containerInfos.append(categoria, titulo, resumo, fonte);
     noticia.append(containerInfos, imagem);
   }
 
